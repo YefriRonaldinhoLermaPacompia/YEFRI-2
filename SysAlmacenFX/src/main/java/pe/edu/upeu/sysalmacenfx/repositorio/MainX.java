3487b9dfdcea64a4ae0450fd18a841e570393f30
YefriRonaldinhoLermaPacompia <ronallerma033@gmail.com>
2024-10-04 11:18:49 -0500

    ALMACEN-CRUD

--- SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/repositorio/MainX.java
+++ SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/repositorio/MainX.java
@@ -4,6 +4,12 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pe.edu.upeu.sysalmacenfx.modelo.Categoria;
+
+import java.util.List;
+import java.util.Scanner;
+
+
+
 @Component
 public class MainX {
 
@@ -23,5 +29,21 @@ public class MainX {
         dd.registro();
     }
 
+    public void listar(){
+        List<Categoria> datos=repository.findAll();
+        System.out.println("ID/Nombre");
+        for(Categoria ca: datos){
+            System.out.println(ca.getIdCategoria()+"\t"+ca.getNombre());
+
+        }
+    }
+    public void menu(){
+        int opc=0;
+        Scanner cs = new Scanner(System.in);
+        String mensaje = "Seleccione una opcion: \n 1=Crear \n 2=Listar \n 3=Salir";
+        System.out.println(mensaje);
+        opc=Integer.parseInt(cs.next());
+
+    }
 
 }
